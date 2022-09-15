1d6d4d28dd1aaaf1c800845c408be41ecf56b290
chikaLabita <itaaa.c2@gmail.com>
2022-09-15 07:55:14 +0700

    blade update

---
+++ pelaporan_ktp/resources/views/data_pelaporan/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.Layout')
+@section('content')
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">NIK</th>
+      <th scope="col">Jenis Pelaporan</th>
+      <th scope="col">Nama</th>
+      <th scope="col">Alamat</th>
+      <th scope="col">Kelurahan</th>
+      <th scope="col">Pengajuan</th>
+      <th scope="col">Keterangan</th>
+      <th scope="col">Operator</th>
+      <th scope="col">Tanggal Registrasi</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
+@endsection
