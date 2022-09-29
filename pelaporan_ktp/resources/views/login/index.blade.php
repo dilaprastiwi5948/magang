7263d0824143bca72e4541af658c76c1103ebb3b
chikaLabita <itaaa.c2@gmail.com>
2022-09-29 12:24:48 +0700

    UI : Login

--- pelaporan_ktp/resources/views/login/index.blade.php
+++ pelaporan_ktp/resources/views/login/index.blade.php
@@ -18,34 +18,39 @@
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
                 @endif
-                <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                <div class="col-sm-10 col-md-5 col-lg-5 mx-auto">
                     <div class="card border-0 shadow rounded-3 my-5">
                         <div class="card-body p-4 p-sm-5">
-                            <h5 class="card-title text-center mb-5 fw-dark fs-5">Sign In</h5>
-                            <form action="loginMultiRoleSession.php" method="post">
-                                <div class="col-md-12 mb-2">
-                                <label>Username</label>
-                                    <input type="text" class="form-control" name="username" placeholder="Username"
-                                        autofocus required>
-                                </div>
-                                <div class="col-md-12 mb-2">
-                                <label>Password</label>
-                                    <input type="password" class="form-control" name="password" placeholder="Password"
-                                        required>
-                                </div>
-                                <div class="d-grid">
-                                    <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Sign
-                                        in</button>
-                                    <p><br>Don't have an account?
-                                        <a class="link-danger" href="/register">Register</a>
-                                    </p>
-                                </div>
-                            </form>
+                                <h5 class="card-title text-center mb-5 fw-dark fs-5">
+                                <img src="https://turbo.net.id/wp-content/webp-express/webp-images/uploads/2020/12/19196952-removebg-preview.png.webp" class="rounded-circle"
+                                    style="width: 150px;" alt="Avatar" /><br>
+                                    SIGN IN
+                                </h5>
+                                <form action="loginMultiRoleSession.php" method="post">
+                                    <div class="col-md-12 mb-2">
+                                        <label>Username</label>
+                                        <input type="text" class="form-control" name="username" placeholder="Username"
+                                            autofocus required>
+                                    </div>
+                                    <div class="col-md-12 mb-2">
+                                        <label>Password</label>
+                                        <input type="password" class="form-control" name="password"
+                                            placeholder="Password" required>
+                                    </div>
+                                    <div class="d-grid">
+                                        <button class="btn btn-primary btn-login text-uppercase fw-bold"
+                                            type="submit">Sign
+                                            in</button>
+                                        <p><br>Don't have an account?
+                                            <a class="link-danger" href="/register">Register</a>
+                                        </p>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 </body>
 
 </html>
