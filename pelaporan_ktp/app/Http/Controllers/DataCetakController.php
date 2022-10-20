eaad7e87c4fa3d7d59fa61ecf2dd7e7e7f558fd2
chikaLabita <itaaa.c2@gmail.com>
2022-10-20 17:16:04 +0700

    Update : DataCetakController

--- pelaporan_ktp/app/Http/Controllers/DataCetakController.php
+++ pelaporan_ktp/app/Http/Controllers/DataCetakController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pelapor;
+use Illuminate\Support\Facades\DB;
 
 class DataCetakController extends Controller
 {
@@ -11,9 +13,10 @@ class DataCetakController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function dataPelaporan()
+    public function index()
     {
-        return view('data_pelaporan.index');
+        $pelapor = DB::table('pelapor')->paginate(5);
+        return view('data_pelaporan.index',  compact('pelapor'));
     }
 
     /**
@@ -23,7 +26,8 @@ class DataCetakController extends Controller
      */
     public function create()
     {
-        //
+        $pelapor = DB::table('pelapor')->paginate(5);
+        return view('data_pelaporan.create', compact('pelapor'));
     }
 
     /**
@@ -34,7 +38,25 @@ class DataCetakController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //melakukan validasi data
+        $request->validate([ 
+            'nik_pelapor' => ['required', 'unique:pelapor'],
+            'jenis_pelaporan' => 'required', 
+            'nama' => 'required', 
+            'tanggal_lahir' => 'required', 
+            'alamat' => 'required', 
+            'kelurahan' => 'required', 
+            'kecamatan' => 'required', 
+            'kota' => 'required', 
+            'pengajuan' => 'required', 
+            'keterangan' => 'required',
+        ]);
+        //fungsi eloquent untuk menambah data 
+        Pelapor::create($request->all());
+
+        //jika data berhasil ditambahkan, akan kembali ke halaman utama
+        return redirect()->route('data_pelaporan.create')
+            ->with('success', 'Data Berhasil Ditambahkan'); 
     }
 
     /**
@@ -43,9 +65,10 @@ class DataCetakController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($nik)
     {
-        //
+        $Pelapor = Pelapor::find($nik_pelapor); 
+        return view('data_pelaporan.detail', compact('Pelapor'));
     }
 
     /**
@@ -54,9 +77,10 @@ class DataCetakController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($nik_pelapor)
     {
-        //
+        $Pelapor = Pelapor::find($nik_pelapor); 
+        return view('data_pelaporan.edit', compact('Pelapor'));
     }
 
     /**
@@ -66,9 +90,28 @@ class DataCetakController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $nik_pelapor)
     {
-        //
+        //melakukan validasi data
+        $request->validate([ 
+            'nik_pelapor' => 'required', 
+            'jenis_pelaporan' => 'required', 
+            'nama' => 'required', 
+            'tanggal_lahir' => 'required', 
+            'alamat' => 'required', 
+            'kelurahan' => 'required', 
+            'kecamatan' => 'required', 
+            'kota' => 'required', 
+            'pengajuan' => 'required', 
+            'keterangan' => 'required', 
+        ]);
+
+        //fungsi eloquent untuk mengupdate data inputan kita 
+        Pelapor::find($nik_pelapor)->update($request->all());
+
+        //jika data berhasil diupdate, akan kembali ke halaman utama 
+        return redirect()->route('data_pelaporan.index') 
+            ->with('success', 'Data Berhasil Diupdate');
     }
 
     /**
@@ -77,8 +120,11 @@ class DataCetakController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($nik_pelapor)
     {
-        //
+         //fungsi eloquent untuk menghapus data 
+         Pelapor::find($nik_pelapor)->delete(); 
+         return redirect()->route('data_pelaporan.index') 
+             -> with('success', 'Data Berhasil Dihapus');
     }
 }
