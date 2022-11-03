33398669491028877be35bf1e30af516580ffdd2
chikaLabita <itaaa.c2@gmail.com>
2022-11-03 10:57:20 +0700

    Edit-Error

--- pelaporan_ktp/resources/views/pelaporan/edit.blade.php
+++ pelaporan_ktp/resources/views/pelaporan/edit.blade.php
@@ -19,63 +19,121 @@
                 <form method="post" action="{{ route('pelaporan.update', $Pelapor->nik_pelapor) }}" id="myForm">
                     @csrf
                     @method('PUT')
-                    <div class="form-group">
-                        <label for="nik_pelapor">NIK</label>
-                        <input type="text" name="nik_pelapor" class="form-control" id="nik_pelapor"
-                            value="{{ $Pelapor->nik_pelapor}}" aria-describedby="nik_pelapor">
-                    </div>
-                    <div class="form-group">
+                    <div class="form-group col-md-12 mb-2">
                         <label for="jenis_pelaporan">Jenis Pelaporan</label>
-                        <select class="form-control" type="jenis_pelaporan" name="jenis_pelaporan" class="form-control" id="jenis_pelaporan"
-                            aria-describedby="jenis_pelaporan" required>
+                        @error('jenis_pelaporan')
+                        <div class="text text-danger mt-2">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                        <select class="form-control" type="jenis_pelaporan" name="jenis_pelaporan" class="form-control"
+                            id="jenis_pelaporan" aria-describedby="jenis_pelaporan">
                             <option disabled value="">Pilih Jenis Pelaporan...</option>
                             <option>Dalam Daerah </option>
                             <option>Luar Daerah</option>
                         </select>
                     </div>
-                    <div class="form-group">
-                        <label for="nama">Nama</label>
-                        <input type="text" name="nama" class="form-control" id="nama" value="{{ $Pelapor->nama }}"
-                            aria-describedby="nama">
+                    <div class="form-row col-md-12 mb-2">
+                        <div class="form-group col-md-6 mb-2">
+                            <label for="nik_pelapor">NIK</label>
+                            @error('nik_pelapor')
+                            <div class="text text-danger mt-2">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                            <input type="text" name="nik_pelapor" class="form-control" id="nik_pelapor"
+                                value="{{ $Pelapor->nik_pelapor}}" aria-describedby="nik_pelapor">
+                        </div>
+                        <div class="form-group col-md-6 mb-2">
+                            <label for="nama">Nama</label>
+                            @error('nama')
+                            <div class="text text-danger mt-2">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                            <input type="text" name="nama" class="form-control" id="nama" value="{{ $Pelapor->nama }}"
+                                aria-describedby="nama">
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="tanggal_lahir">Tanggal Tahir</label>
-                        <input type="date" name="tanggal_lahir" class="form-control" id="tanggal_lahir"
-                            value="{{ $Pelapor->tanggal_lahir }}" aria-describedby="tanggal_lahir" required>
+                    <div class="form-row col-md-12 mb-2">
+                        <div class="form-group col-md-6 mb-2">
+                            <label for="tanggal_lahir">Tanggal Tahir</label>
+                            @error('tanggal_lahir')
+                            <div class="text text-danger mt-2">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                            <input type="date" name="tanggal_lahir" class="form-control" id="tanggal_lahir"
+                                value="{{ $Pelapor->tanggal_lahir }}" aria-describedby="tanggal_lahir">
+                        </div>
+                        <div class="form-group col-md-6 mb-2">
+                            <label for="alamat">Alamat</label>
+                            @error('alamat')
+                            <div class="text text-danger mt-2">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                            <input type="alamat" name="alamat" class="form-control" id="alamat"
+                                aria-describedby="alamat" value="{{ $Pelapor->alamat }}">
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="alamat">Alamat</label>
-                        <input type="alamat" name="alamat" class="form-control" id="alamat" aria-describedby="alamat"
-                            required value="{{ $Pelapor->alamat }}">
+                    <div class="form-row col-md-12 mb-2">
+                        <div class="form-group col-md-6 mb-2">
+                            <label for="kelurahan">Desa / Kelurahan</label>
+                            @error('kelurahan')
+                            <div class="text text-danger mt-2">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                            <input type="kelurahan" name="kelurahan" class="form-control" id="kelurahan"
+                                aria-describedby="kelurahan" value="{{ $Pelapor->kelurahan }}">
+                        </div>
+                        <div class="form-group col-md-6 mb-2">
+                            <label for="kecamatan">Kecamatan</label>
+                            @error('kecamatan')
+                            <div class="text text-danger mt-2">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                            <input type="kecamatan" name="kecamatan" class="form-control" id="kecamatan"
+                                aria-describedby="kecamatan" value="{{ $Pelapor->kecamatan }}">
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="kelurahan">Desa / Kelurahan</label>
-                        <input type="kelurahan" name="kelurahan" class="form-control" id="kelurahan"
-                            aria-describedby="kelurahan" required value="{{ $Pelapor->kelurahan }}">
+                    <div class="form-row col-md-12 mb-2">
+                        <div class="form-group col-md-6 mb-2">
+                            <label for="kota">Kabupaten / Kota</label>
+                            @error('kota')
+                            <div class="text text-danger mt-2">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                            <input type="kota" name="kota" class="form-control" id="kota" aria-describedby="kota"
+                                value="{{ $Pelapor->kota }}">
+                        </div>
+                        <div class="form-group col-md-6 mb-2">
+                            <label for="pengajuan">Pengajuan</label>
+                            @error('pengajuan')
+                            <div class="text text-danger mt-2">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                            <select type="pengajuan" name="pengajuan" class="form-control" id="pengajuan"
+                                aria-describedby="pengajuan" value="{{ $Pelapor->pengajuan }}">
+                                <option disabled value="">Pilih Jenis Pengajuan...</option>
+                                <option>Online</option>
+                                <option>Dinas</option>
+                                <option>Kelurahan</option>
+                                <option>MPP</option>
+                            </select>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="kecamatan">Kecamatan</label>
-                        <input type="kecamatan" name="kecamatan" class="form-control" id="kecamatan"
-                            aria-describedby="kecamatan" required value="{{ $Pelapor->kecamatan }}">
-                    </div>
-                    <div class="form-group">
-                        <label for="kota">Kabupaten / Kota</label>
-                        <input type="kota" name="kota" class="form-control" id="kota" aria-describedby="kota" required
-                            value="{{ $Pelapor->kota }}">
-                    </div>
-                    <div class="form-group">
-                        <label for="pengajuan">Pengajuan</label>
-                        <select type="pengajuan" name="pengajuan" class="form-control" id="pengajuan"
-                            aria-describedby="pengajuan" value="{{ $Pelapor->pengajuan }}">
-                            <option disabled value="">Pilih Jenis Pengajuan...</option>
-                            <option>Online</option>
-                            <option>Dinas</option>
-                            <option>Kelurahan</option>
-                            <option>MPP</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
+                    <div class="form-group col-md-12 mb-2">
                         <label for="keterangan">Keterangan</label>
+                        @error('keterangan')
+                        <div class="text text-danger mt-2">
+                            {{ $message }}
+                        </div>
+                        @enderror
                         <select type="keterangan" name="keterangan" class="form-control" id="keterangan"
                             aria-describedby="keterangan" value="{{ $Pelapor->keterangan }}">
                             <option disabled value="">Pilih Keterangan...</option>
@@ -87,9 +145,9 @@
                             <option>Surat Keterangan</option>
                         </select>
                     </div>
-                    <div class="d-grid gap-2 d-md-block">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                        <a class="btn btn-success" href="{{ route('pelaporan.index') }}">Back</a>
+                    <div class="from-group col-md-12 d-grid gap-2 d-md-block">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                        <a class="btn btn-success" href="{{ route('pelaporan.index') }}">Kembali</a>
                     </div>
                 </form>
             </div>
