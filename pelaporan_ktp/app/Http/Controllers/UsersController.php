2304fb8e9cf8c23025c287a0ccf2a204ddd34c1c
chikaLabita <itaaa.c2@gmail.com>
2022-11-03 11:02:02 +0700

    Edit : UsersController

--- pelaporan_ktp/app/Http/Controllers/UsersController.php
+++ pelaporan_ktp/app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
@@ -13,7 +14,8 @@ class UsersController extends Controller
      */
     public function index()
     {
-        return view('users.index');
+        $users = DB::table('users');
+        return view('users.index', compact('users'));
     }
 
     /**
